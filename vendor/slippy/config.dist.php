9874e58fb09d5a1c49287c02342f6c8aa75542bf
Tomotaka Sakuma <ktmtmks@gmail.com>
2012-01-06 07:56:51 +0900

    To be submodule which are on github.com.

--- vendor/slippy/config.dist.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// directory where the slide decks are placed (*.html is scanned)
-$dir = dirname(__FILE__);
-
-// the php template that will render the base repository page
-$repositoryTemplate = 'repo.php';
