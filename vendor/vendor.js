a016308db03c79ee32f790d29887bbf25fbe8f89
Tomotaka Sakuma <ktmtmks@gmail.com>
2012-02-05 16:38:49 +0900

    -

--- vendor/vendor.js
+++ vendor/vendor.js
@@ -3,3 +3,4 @@
 //= require           jasmine/lib/jasmine-core/jasmine
 //= require           jasmine/lib/jasmine-core/jasmine-html
 //= require           jasmine-story/src/core/jasmine-story
+//= require bootstrap/docs/assets/js/bootstrap-tab.js
