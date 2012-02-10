31c7232c8ddbc182cec13ec0ddddbbec7f15d33d
Tomotaka Sakuma <ktmtmks@gmail.com>
2012-02-10 10:31:31 +0900

    Note.

--- assets/javascripts/jumly.js
+++ assets/javascripts/jumly.js
@@ -1,3 +1,5 @@
+//NOTE: This sprockets file is used in Rakefile to build script.
+//      Don't move this file, and don't change following order.
 //= require jquery-ext
 //= require jquery-g2d
 //= require core
