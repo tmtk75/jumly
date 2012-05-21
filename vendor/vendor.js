86563a686a03608e611ee1a5e63920d2ae70c945
Tomotaka Sakuma <ktmtmks@gmail.com>
2012-05-21 09:14:09 +0900

    using-npm

--- vendor/vendor.js
+++
@@ -1,6 +0,0 @@
-//= require           jquery.min
-//= require_directory .
-//= require           jasmine/lib/jasmine-core/jasmine
-//= require           jasmine/lib/jasmine-core/jasmine-html
-//= require           jasmine-story/src/core/jasmine-story
-//= require bootstrap/docs/assets/js/bootstrap-tab.js
