e382d593374a426cf9ef5d8a2894a34984855073
Tomotaka Sakuma <ktmtmks@gmail.com>
2012-09-18 22:31:37 +0900

    take syntaxhighlighter

---
+++ views/static/syntaxhighlighter.js
@@ -0,0 +1,5 @@
+$(function() {
+    SyntaxHighlighter.config.clipboardSwf = './syntaxhighlighter/scripts/clipboard.swf';
+    SyntaxHighlighter.config.toolbar = false;
+    SyntaxHighlighter.all()
+});
