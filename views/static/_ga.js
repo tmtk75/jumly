87ed997707f1bd24bcfb1536115c5c32781a35bd
Tomotaka Sakuma <ktmtmks@gmail.com>
2012-12-07 12:02:05 +0900

    change tracking code of GA

--- views/static/_ga.js
+++ views/static/_ga.js
@@ -1,5 +1,5 @@
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-36257845-1']);
+_gaq.push(['_setAccount', 'UA-27755043-3']);
 _gaq.push(['_trackPageview']);
 (function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
