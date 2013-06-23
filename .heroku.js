33664dabbe71bf2340c17614c75c1af7fcb4c105
Tomotaka Sakuma <ktmtmks@gmail.com>
2013-06-23 17:16:27 +0900

    clustering (pre-fork)

--- .heroku.js
+++ .heroku.js
@@ -1,6 +1,17 @@
 // $ heroku create --stack cedar
 // $ heroku config:add NODE_ENV=heroku
-var coffee = require('coffee-script');
-var fs = require('fs');
-var app = coffee.compile(fs.readFileSync('./app.coffee').toString());
-eval(app);
+
+var cluster = require('cluster');
+var prefork_count = require('os').cpus().length;
+
+if (cluster.isMaster) {
+  console.log("prefork-count:", prefork_count);
+  for (var i = 0; i < prefork_count; i++) {
+    cluster.fork();
+  }
+} else {
+  var coffee = require('coffee-script');
+  var fs = require('fs');
+  var app = coffee.compile(fs.readFileSync('./app.coffee').toString());
+  eval(app);
+}
